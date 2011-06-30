e6817829c07484ab577b277d9e8e87728bb45b83
b <b@e643a83d-2a85-430c-918e-6eb48f6a02dc>
2011-06-30 17:17:29 +0000

    Documentation en cours d'écriture
    git-svn-id: https://pandab.fr/svn/cacofonix@69 e643a83d-2a85-430c-918e-6eb48f6a02dc

--- doc/make.bat
+++ doc/make.bat
@@ -1,7 +1,4 @@
 
-set PATH=C:\Program Files\LilyPond\usr\bin;%PATH%
-
-REM "C:\Program Files\LilyPond\usr\bin\lilypond-book.py" --format=latex --output=out --lily-output-dir=out -V cacofonix.tex
 lilypond-book.py --format=latex --output=out --lily-output-dir=out -V cacofonix.tex
 pause
 
