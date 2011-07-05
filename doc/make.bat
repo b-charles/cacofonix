0ba20c1a27e5e3d727e80c0901d272558de233c2
b <b@e643a83d-2a85-430c-918e-6eb48f6a02dc>
2011-07-05 14:17:29 +0000

    MAJ documentation
    git-svn-id: https://pandab.fr/svn/cacofonix@74 e643a83d-2a85-430c-918e-6eb48f6a02dc

--- doc/make.bat
+++ doc/make.bat
@@ -1,11 +1,10 @@
 
-lilypond-book.py --format=latex --output=out --lily-output-dir=out -V cacofonix.tex
-pause
+lilypond-book.py --format=latex --output=out --lily-output-dir=out -V cacofonix.tex || pause
 
 cd out
 latex cacofonix.tex
 latex cacofonix.tex
-pause
+latex cacofonix.tex || pause
 
 dvips cacofonix.dvi
 copy cacofonix.ps ..\
