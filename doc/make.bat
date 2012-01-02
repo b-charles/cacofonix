bd94565487df68bdfa2aa8a97d51355ed3ddb7d0
b <b@e643a83d-2a85-430c-918e-6eb48f6a02dc>
2012-01-02 16:16:18 +0000

    MAJ doc: ajout operator -
    git-svn-id: https://pandab.fr/svn/cacofonix@161 e643a83d-2a85-430c-918e-6eb48f6a02dc

--- doc/make.bat
+++ doc/make.bat
@@ -1,5 +1,7 @@
 REM To create the documentation, you need LaTeX, Lilypond and Asymptote.
-REM Then, run this script (don't forget to add lilypond and asymptote executables in your path)
+REM Don't forget to add lilypond and asymptote executables in your path.
+REM Don't forget to add asymptote.sty in the MikTeX path
+REM Then, run this script
 REM The complete path to the cacofonix folder can't contain space. 
 
 lilypond-book.py --format=latex --output=out --lily-output-dir=out cacofonix.tex || pause
